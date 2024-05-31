01f42bb8d4c9ceb09aac345c0c3ef76a78fafa88
_creare_ <33350279+1-creare-1@users.noreply.github.com>
2024-05-31 12:44:18 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,10 @@
+local test = true;
 
+local res = fetch({
+    url = "https://api.buss.lol/",
+    method = "GET",
+    headers = { ["Content-Type"] = "application/json" },
+    body = '{ "test": ' .. test .. '}'
+})
+
+get("output").set_content(res)
